419236d6e821ab0b614bc61cef2bd665b25352c7
Adam Essmyer <adamessmyer@Adams-MacBook-Pro.local>
2025-05-08 07:27:01 -0500

    added function that checks for duplicates, added database module that defines the DB_CUSTOMERS constant and imported into main.py.

---
+++ database.py
@@ -0,0 +1,4 @@
+import os
+
+# Define the path to the customers database file
+DB_CUSTOMERS = os.path.join(os.path.dirname(__file__), 'customer_data.txt') 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import database
+
 def main_menu():
     global clear_needed
     if clear_needed:
@@ -27,6 +29,24 @@ def main_menu():
                 cin = random_CIN(8)
                 if not is_duplicate_cin(cin):
                     break;
+
+    def is_duplicate_cin(cin_to_check):
+        try:
+            with open(database.DB_CUSTOMERS, 'r') as file:
+                for line in file:
+                    cin = line.strip().split(",")[0]
+                    if cin == cin_to_check:
+                        print("Duplicate CIN found. Please try again.")
+                        return True
+            return False
+        except FileNotFoundError:
+            return False
+        except Exception as e:
+            print(f"An error occurred while checking for duplicate CIN: {e}")
+            return True
+
+
+                            
 
     
     
